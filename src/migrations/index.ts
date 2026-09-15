837199a14362ea0343dc3a1b84e6c087f6ebd4f5
Svechnikov Yaroslav <tula18051@gmail.com>
2026-09-15 12:23:16 +0500

    fix(storage): add media prefix migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260913_152037_initial from './20260913_152037_initial';
+import * as migration_20260915_072232_add_media_prefix from './20260915_072232_add_media_prefix';
 
 export const migrations = [
   {
     up: migration_20260913_152037_initial.up,
     down: migration_20260913_152037_initial.down,
-    name: '20260913_152037_initial'
+    name: '20260913_152037_initial',
+  },
+  {
+    up: migration_20260915_072232_add_media_prefix.up,
+    down: migration_20260915_072232_add_media_prefix.down,
+    name: '20260915_072232_add_media_prefix'
   },
 ];
